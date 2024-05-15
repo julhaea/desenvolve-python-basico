6b92811e8222f5a461bac2529771ef30319d99d6
julhaea <juliaferaramis@gmail.com>
2024-05-15 12:54:56 -0300

    7.1 - Strings

--- Módulo 7 - Strings e arquivos/aula1_questao7.py
+++ Módulo 7 - Strings e arquivos/aula1_questao7.py
@@ -1,17 +1,13 @@
 import random
-def encrypt(lista):
-    chave=random.randint(0,10)
+def encrypt(lista,chave):
     nome=""
     for i in lista:
         nova_letra=chr(ord(i)+chave)
         nome+=nova_letra
     return nome
-
-
-
 nomes = ["Luana", "Ju", "Davi", "Vivi", "Pri", "Luiz"]
-nomes_cripto=list(map(encrypt,nomes))
+chave=random.randint(0,10)
+nomes_cripto=list(map(encrypt,nomes,[chave]*len(nomes)))
 print("Nomes:",nomes)
-chave=ord(nomes_cripto[0][0])-ord(nomes[0][0])
 print("Chave aleatória:", chave)
 print("Nomes criptografados:",nomes_cripto)
