8b3a2870609523c57f842e5d01b3f04cf01de794
julhaea <juliaferaramis@gmail.com>
2024-05-15 22:51:29 -0300

    7.2 - Operações

--- Módulo 7 - Strings e arquivos/aula2_questao5.py
+++ Módulo 7 - Strings e arquivos/aula2_questao5.py
@@ -3,12 +3,15 @@ def embaralhar_frase(frase):
     palavras = frase.split()
     palavras_embaralhadas = []
     for palavra in palavras:
-        primeiraLetra = palavra[0]
-        letrasInternas = list(palavra[1:-1])
-        random.shuffle(letrasInternas)
-        ultimaLetra = palavra[-1]
-        palavra_embaralhada = primeiraLetra + ''.join(letrasInternas) + ultimaLetra
-        palavras_embaralhadas.append(palavra_embaralhada)
+        if len(palavra)>1:
+            primeiraLetra = palavra[0]
+            letrasInternas = list(palavra[1:-1])
+            random.shuffle(letrasInternas)
+            ultimaLetra = palavra[-1]
+            palavra_embaralhada = primeiraLetra + ''.join(letrasInternas) + ultimaLetra
+            palavras_embaralhadas.append(palavra_embaralhada)
+        else:
+            palavras_embaralhadas.append(palavra)
     frase_embaralhada =' '.join(palavras_embaralhadas)
     return frase_embaralhada
 frase = input("Digite uma frase: ")
